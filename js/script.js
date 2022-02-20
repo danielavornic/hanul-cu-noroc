fa5e06a92e2211a67c7c0a3651b756bf5b8909fc
Daniela Vornic <daniela.vornic@gmail.com>
2022-02-20 23:51:23 +0200

    create header and add all necessary files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+  const hideMenu = () => {
+    if (
+      window.matchMedia('(max-width: 768px)').matches &&
+      !$('#toggler').is(':checked')
+    )
+      $('.menu').hide();
+    else $('.menu').show();
+  };
+  hideMenu();
+  $(window).on('resize', hideMenu);
+
+  $('#toggler').click(() => $('.menu').slideToggle());
+});
